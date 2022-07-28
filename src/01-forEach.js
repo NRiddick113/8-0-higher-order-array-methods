a82bc07f922ec5ef7e4faaccad9b246e5c4b9f12
NRiddick113 <NatashaRiddick@pursuit.org>
2022-07-28 13:57:23 -0400

    forEach

--- src/01-forEach.js
+++ src/01-forEach.js
@@ -12,7 +12,13 @@ const exampleSongData = require("../data/songs");
  * Logs out all of the song titles.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printAllSongTitles(songs) {}
+function printAllSongTitles(songs) {
+  songs.forEach((songs) => {
+    console.log(songs.title)
+  })
+  
+
+}
 
 /**
  * Logs out the song title and song artist.
@@ -24,13 +30,24 @@ function printAllSongTitles(songs) {}
  *  //> "Up by Sebastian Kamae"
  *  //> ...
  */
-function printSongDetails(songs) {}
+function printSongDetails(songs) {
+  songs.forEach((songs) => {
+    console.log(`${songs.title} by ${songs.artist}`)
+  })
+}
 
 /**
  * Logs out all of the song titles which have a runtime over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printSongTitlesOverThreeMinutes(songs) {}
+function printSongTitlesOverThreeMinutes(songs) {
+  songs.forEach((songs, i) => {
+    if (songs.runTimeInSeconds > 180) {
+      songs[i] = songs.title
+    }
+    console.log(songs.title)
+  })
+}
 
 module.exports = {
   printAllSongTitles,
